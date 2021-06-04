be4b94dfe568e892e534c32e0c334963da3b4e8d
Vinicius <viniciusaugustobenedito@gmail.com>
2021-06-03 21:42:30 -0300

    Criação da estrutura MVC com carregamento de páginas, criação da página 404 e validações internas.

---
+++ DankiCode/Views/pages/404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opss... A página não existe!</title>
+</head>
+<body>
+    <h2>Opss... A página não existe!</h2>
+</body>
+</html>
