deb04df62ff56f42cf978352fc59a0d01ea925bc
Vinicius Benedito <viniciusaugustobenedito@gmail.com>
2022-04-16 16:24:58 -0300

    Criação do sistema de login.

--- DankiCode/Controllers/HomeController.php
+++ DankiCode/Controllers/HomeController.php
@@ -10,6 +10,35 @@
                 \DankiCode\Views\MainView::render('home');
             } else {
                 // Renderizar para criar conta.
+
+                if (isset($_POST['login'])) {
+                    $login = $_POST['email'];
+                    $senha = $_POST['senha'];
+
+                    // Verificar no banco de dados.
+
+                    $verifica = \DankiCode\MySql::connect()->prepare("SELECT * FROM usuarios WHERE email = ?");
+                    $verifica->execute(array($login));
+
+                    if ($verifica->rowCount() == 0) {
+                        // Não existe o usuário!
+                        \DankiCode\Utilidades::alerta('Não existe nenhum usuário com este e-mail...');
+                        \DankiCode\Utilidades::redirect(INCLUDE_PATH);
+                    } else {
+                        $dados = $verifica->fetch();
+                        $senhaBanco = $dados['senha'];
+                        if (\DankiCode\Bcrypt::check($senha,$senhaBanco)) {
+                            // Usuário logado com sucesso
+                            $_SESSION['login'] = $dados['email'];
+                            \DankiCode\Utilidades::alerta('Login realizado com sucesso!');
+                            \DankiCode\Utilidades::redirect(INCLUDE_PATH);
+                        } else {
+                            \DankiCode\Utilidades::alerta('Senha incorreta...');
+                            \DankiCode\Utilidades::redirect(INCLUDE_PATH);
+                        }
+                    }
+                }
+
                 \DankiCode\Views\MainView::render('login');
             }
         }
